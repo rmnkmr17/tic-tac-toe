145345be49a385cb767a3f63b35da674ac14dcd4
rmkkmr17 <ramkum94@tutanota.com>
2023-02-05 17:12:17 +0100

    Added the function to render content and add marks.

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-const people = 34;
+// gameBoard Module
+const gameBoardModule = (function () {
+  let gameBoardArray = ["", "", "", "", "", "", "", ""];
+  return { gameBoardArray };
+})();
 
-function doSomething() {
-  people = 324;
-}
+// displayController Module
+const displayController = (function () {
+  let testF = console.log("test displayController");
+  return { testF };
+})();
 
-console.log(people);
+// player Factory Function
+const player = (name, mark, turn) => {
+  return { name, mark, turn };
+};
+
+const playerOne = player("One", "X", true);
+const playerTwo = player("Two", "O", false);
+
+console.log((playerOne.name = "John"));
+console.log(playerOne.name);
+
+// function to render content of the gameboard
+
+const renderContent = () => {
+  const boxes = document.querySelectorAll(".row");
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].textContent = gameBoardModule.gameBoardArray[i];
+  }
+};
+
+// add marks to the screen
+const addMarks = () => {
+  const boxes = document.querySelectorAll(".row");
+  boxes.forEach((box) => {
+    box.addEventListener("click", (e) => {
+      const box = e.target;
+      const boxIndex = box.getAttribute("data-index-number");
+      const mark = playerOne.mark;
+      gameBoardModule.gameBoardArray.splice(boxIndex, 1, mark);
+      renderContent();
+      console.log(gameBoardModule.gameBoardArray);
+    });
+  });
+};
+
+addMarks();
