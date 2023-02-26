14ea833074c924dfef87fc12adaf8a6230062be9
rmkkmr17 <ramkum94@tutanota.com>
2023-02-26 21:19:16 +0100

    Added the render function

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
-// gameBoard Module
-const gameBoardModule = (function () {
-  let gameBoardArray = ["", "", "", "", "", "", "", ""];
-  return { gameBoardArray };
-})();
-
-// displayController Module
-const displayController = (function () {
-  let testF = console.log("test displayController");
-  return { testF };
-})();
-
-// player Factory Function
-const player = (name, mark, turn) => {
-  return { name, mark, turn };
-};
-
-const playerOne = player("One", "X", true);
-const playerTwo = player("Two", "O", false);
-
-console.log((playerOne.name = "John"));
-console.log(playerOne.name);
-
-// function to render content of the gameboard
-
-const renderContent = () => {
-  const boxes = document.querySelectorAll(".row");
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].textContent = gameBoardModule.gameBoardArray[i];
-  }
-};
-
-// add marks to the screen
-const addMarks = () => {
-  const boxes = document.querySelectorAll(".row");
-  boxes.forEach((box) => {
-    box.addEventListener("click", (e) => {
-      const box = e.target;
-      const boxIndex = box.getAttribute("data-index-number");
-      const mark = playerOne.mark;
-      gameBoardModule.gameBoardArray.splice(boxIndex, 1, mark);
-      renderContent();
-      console.log(gameBoardModule.gameBoardArray);
+const Gameboard = (() => {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
+  const render = () => {
+    let boardHTML = "";
+    gameboard.forEach((square, index) => {
+      boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
     });
-  });
+    document.querySelector("#gameboard").innerHTML = boardHTML;
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", Game.handleClick);
+    });
+  };
+
+  const update = (index, value) => {
+    gameboard[index] = value;
+    render();
+  };
+  return { render, update };
+})();
+
+const createPlayer = (name, mark) => {
+  return { name, mark };
 };
 
-addMarks();
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector("#player1").value, "X"),
+      createPlayer(document.querySelector("#player2").value, "O"),
+    ];
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", Game.handleClick);
+    });
+  };
+
+  const handleClick = (e) => {
+    let index = parseInt(e.target.id[e.target.id.length - 1]);
+    Gameboard.update(index, players[currentPlayerIndex].mark);
+
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+  };
+  return { start, handleClick };
+})();
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+  Game.start();
+});
