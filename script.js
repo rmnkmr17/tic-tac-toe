f3a6d79521af82ff3322fbdb04114d30c3c1c1eb
rmkkmr17 <ramkum94@tutanota.com>
2023-02-27 18:05:25 +0100

    Added the function to check the win

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ const Gameboard = (() => {
     gameboard[index] = value;
     render();
   };
-  return { render, update };
+
+  const getGameboard = () => gameboard;
+
+  return { render, update, getGameboard };
 })();
 
 const createPlayer = (name, mark) => {
@@ -44,12 +47,52 @@ const Game = (() => {
 
   const handleClick = (e) => {
     let index = parseInt(e.target.id[e.target.id.length - 1]);
+    if (Gameboard.getGameboard()[index] !== "") {
+      return;
+    }
     Gameboard.update(index, players[currentPlayerIndex].mark);
 
+    if (
+      checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)
+    ) {
+      gameOver = true;
+      alert(`${players[currentPlayerIndex].name} won`);
+    }
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
   };
-  return { start, handleClick };
+
+  const restartGame = () => {
+    for (let i = 0; i < 9; i++) {
+      Gameboard.update(i, "");
+    }
+    Gameboard.render();
+  };
+  return { start, handleClick, restartGame };
 })();
+
+function checkForWin(board) {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombos.length; i++) {
+    const [a, b, c] = winningCombos[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click", Game.restartGame);
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
